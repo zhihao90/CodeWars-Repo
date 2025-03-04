7b93c19f905a0cb5615d9932f165c10f46809f81
Wang Zhihao <zhihao90@gmail.com>
2025-03-04 10:44:26 -0800

    5k - Generates Hashtags with a ruleset

---
+++ 5kyu-Challenges/generateHash.js
@@ -0,0 +1,47 @@
+/*
+The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+Examples
+" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+"    Hello     World   "                  =>  "#HelloWorld"
+""                                        =>  false
+*/
+
+
+function generateHashtag (str) {
+
+    if(str.trim() == "") return false;
+
+    let result = []
+
+    str = str.trim().split(" ");
+
+    for (let item of str){
+
+        if(item != ""){
+            item = item.split("")
+            item[0] = item[0].toUpperCase();
+            result.push(item.join(""))
+        }
+
+
+    }
+
+    final = `#${result.join("")}`
+
+    return final.length <= 140 ? final : false;
+
+
+
+}
+
+
+
+
